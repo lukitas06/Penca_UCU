2a3c4e138e830eea2dde49a6c5e9ab881133a738
lukitas06 <lucasalegre13@gmail.com>
2024-05-15 11:05:47 -0300

    formValidation & schemas

---
+++ penca_ucu/app/lib/teams.ts
@@ -0,0 +1,23 @@
+import z from "zod"
+
+export const TeamSchema:any = z.object({
+
+    name: z.string().min(2, "El nombre del equipo debe tener al menos 2 caracteres").trim(),
+    group: z.string().min(1, "El grupo del equipo debe tener al menos 1 caracter").trim(),
+    flag: z.string().min(2, "La bandera del equipo debe tener al menos 2 caracteres").trim(),
+    fifa_ranking: z.number().int().positive("El ranking FIFA debe ser un número positivo"),
+    group_position: z.number().int().positive("La posición en el grupo debe ser un número positivo"),
+    points: z.number().int().positive("Los puntos deben ser un número positivo"),
+    goals_scored: z.number().int().positive("Los goles a favor deben ser un número positivo"),
+    goals_against: z.number().int().positive("Los goles en contra deben ser un número positivo"),
+    goal_difference: z.number().int().positive("La diferencia de goles debe ser un número positivo"),
+    matches_played: z.number().int().positive("Los partidos jugados deben ser un número positivo"),
+    wins: z.number().int().positive("Las victorias deben ser un número positivo"),
+    draws: z.number().int().positive("Los empates deben ser un número positivo"),
+    losses: z.number().int().positive("Las derrotas deben ser un número positivo"),
+    team_id: z.number().int().positive("El id del equipo debe ser un número positivo"),
+    tournament_id: z.number().int().positive("El id del torneo debe ser un número positivo"),
+    created_at: z.string(),
+    updated_at: z.string(),
+    deleted_at: z.string(),
+})
---
+++ penca_ucu/app/pages/login/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React from "react"
+
+import LoginRegisterCard  from "../../ui/components/LoginRegisterCard"
+
+export default function Login(){
+
+    
+    return(
+        <LoginRegisterCard />
+    )
+}
