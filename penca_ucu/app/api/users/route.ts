cf026940e8a44608d5242580e92b8dfe337316ad
HERNÁN CIARLO <ciarlohernan@gmail.com>
2024-06-25 17:41:04 -0300

    arreglo merge transaccionalidad

--- penca_ucu/app/api/users/route.ts
+++ penca_ucu/app/api/users/route.ts
@@ -26,24 +26,29 @@ export async function POST(req: NextRequest) {
     try {
         const body = await req.json();
         const { usuario, nombres, apellidos, email, contrasena, carrera, primer_lugar, segundo_lugar } = body;
-        
-        const userExists = await checkIfUserExists(usuario);
-        const emailExists = await checkIfEmailExists(email);
+
+        const userExists = await withTransaction(async (connection: PoolConnection) => {
+            return checkIfUserExists(connection, usuario);
+        });
+
+        const emailExists = await withTransaction(async (connection: PoolConnection) => {
+            return checkIfEmailExists(connection, email);
+        });
 
         if (userExists) {
-            return new Response(
-                JSON.stringify({ message: "User already exists" }),
+            return NextResponse.json(
+                { message: "User already exists" },
                 { status: 400 }
             );
         }
-      
+
         if (emailExists) {
-            return new Response(
-                JSON.stringify({ message: "Email already exists" }),
+            return NextResponse.json(
+                { message: "Email already exists" },
                 { status: 400 }
             );
         }
-      
+
         const query = `INSERT INTO Usuario (usuario, nombres, apellidos, email, contrasena, es_admin, puntaje, carrera, primer_lugar, segundo_lugar) VALUES (?, ?, ?, ?, ?, FALSE, 0, ?, ?, ?);`;
 
         return await withTransaction(async (connection: PoolConnection) => {
@@ -67,7 +72,7 @@ export async function PUT(req: NextRequest) {
         console.log("Usuario", usuario);
         const body = await req.json();
         const { puntaje } = body;
-        console.log("puntaje", puntaje)
+        console.log("puntaje", puntaje);
 
         const query = `UPDATE Usuario SET puntaje = puntaje + ? WHERE usuario = ?`;
 
@@ -99,38 +104,14 @@ const getUsers = async (connection: PoolConnection): Promise<any> => {
     return results;
 };
 
-const checkIfUserExists = (username: string): Promise<boolean> => {
+const checkIfUserExists = async (connection: PoolConnection, username: string): Promise<boolean> => {
     const checkUsernameQuery = `SELECT * FROM Usuario WHERE usuario = ?`;
+    const [results] = await connection.execute(checkUsernameQuery, [username]);
+    return (results as RowDataPacket[]).length > 0;
+};
 
-    return new Promise<boolean>((resolve, reject) => {
-        connection.query(checkUsernameQuery, [username], (err, results: RowDataPacket[]) => {
-            if (err) {
-                console.log("Error checking if user exists", err);
-                reject(true)
-            }
-            if (results.length > 0) {
-                console.log("User already exists", results);
-                resolve(true);
-            }
-            resolve(false);
-        });
-    })
-}
-
-const checkIfEmailExists = (email: string): Promise<boolean> => {
-    const checkUsernameQuery = `SELECT * FROM Usuario WHERE email = ?`;
-
-    return new Promise<boolean>((resolve, reject) => {
-        connection.query(checkUsernameQuery, [email], (err, results: RowDataPacket[]) => {
-            if (err) {
-                console.log("Error checking if email exists", err);
-                reject(true)
-            }
-            if (results.length > 0) {
-                console.log("Email already exists", results);
-                resolve(true);
-            }
-            resolve(false);
-        });
-    })
-}
+const checkIfEmailExists = async (connection: PoolConnection, email: string): Promise<boolean> => {
+    const checkEmailQuery = `SELECT * FROM Usuario WHERE email = ?`;
+    const [results] = await connection.execute(checkEmailQuery, [email]);
+    return (results as RowDataPacket[]).length > 0;
+};
