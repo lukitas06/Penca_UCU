baa94101753c31e40445073b06cfc9ed0310ca88
HERNÁN CIARLO <ciarlohernan@gmail.com>
2024-06-25 19:12:24 -0300

    containerización funcionando

--- penca_ucu/app/lib/dbConnection.ts
+++ penca_ucu/app/lib/dbConnection.ts
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-    host: 'localhost',
+    host: process.env.DB_HOST,
     port: 3306,
     database: 'penca_ucu',
     user: 'root',
